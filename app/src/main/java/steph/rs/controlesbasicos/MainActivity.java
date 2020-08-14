405369700771ce9f6c1bf6eafa2e681a50283580
Hyung97 <tammyhyunglee@gmail.com>
2020-08-14 11:27:45 -0600

    Terminacion y correccion de modulo/residuo

--- app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
+++ app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             double num2 = Double.parseDouble(tempval.getText().toString());
 
             double respuesta = 0;
-            //Este es para el radiogroup y los radiobuttons
+            //Este es para el RadioGroup y los RadioButtons
             switch (optOperaciones.getCheckedRadioButtonId()) {
                 case R.id.optSuma:
                     respuesta = num1 + num2;
@@ -50,14 +50,15 @@ public class MainActivity extends AppCompatActivity {
                     respuesta = Math.pow(num1, num2);
                     break;
                 case R.id.optModulo:
-                    respuesta = num1- (num1 / num2 * num2);
+                    respuesta = num1%num2;
                     break;
                 case R.id.optFactoreo:
-                    respuesta = num1 / num2;
+                    long factorial= 3;
+                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-factorial);
                     break;
 
         }
-            //Este es para el spinner... -> ComboBox.
+            //Este es para el Spinner... -> ComboBox.
             switch (cboOperaciones.getSelectedItemPosition()){
                 case 1: //suma
                     respuesta = num1 + num2;
@@ -78,11 +79,11 @@ public class MainActivity extends AppCompatActivity {
                     respuesta = Math.pow(num1, num2);
                     break;
                 case 7: //modulo
-                    respuesta = num1- (num1 / num2 * num2);
+                    respuesta = num1%num2;
                     break;
                 case 8: //factoreo
-                    long factorial= 1;
-                    respuesta = (num1 + num2) * factorial;
+                    long factorial = 3;
+                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-factorial);
                     break;
             }
             tempval = (TextView) findViewById(R.id.lblRespuesta);
