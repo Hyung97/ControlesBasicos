5781b60f753f6b6ed75005e007c6f5b28020dcd3
Hyung97 <tammyhyunglee@gmail.com>
2020-08-27 18:11:01 -0600

    Trabajando con los tabhost

--- app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
+++ app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
@@ -4,96 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //direcciones direccion = new direcciones();
+    dirrecciones_paises miDireccion = new dirrecciones_paises();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void calcular(View view) {
-        try {
-            RadioGroup optOperaciones = (RadioGroup) findViewById(R.id.optOperaciones);
-            Spinner cboOperaciones = (Spinner) findViewById(R.id.cboOperaciones);
+        TabHost tbhConversores = (TabHost)findViewById(R.id.tbhDirecciones);
+        tbhConversores.setup();
 
-            TextView tempval = (TextView) findViewById(R.id.txtNum1);
-            double num1 = Double.parseDouble(tempval.getText().toString());
+        tbhConversores.addTab(tbhConversores.newTabSpec("Direccion").setContent(R.id.tabDireccion).setIndicator("DIRECCION", null));
+        tbhConversores.addTab(tbhConversores.newTabSpec("Pais").setContent(R.id.tabPais).setIndicator("PAIS", null));
 
-            tempval = (TextView) findViewById(R.id.txtNum2);
-            double num2 = Double.parseDouble(tempval.getText().toString());
+        final Spinner spnMun = (Spinner)findViewById(R.id.spnMun);
 
-            double respuesta = 0;
-            //Este es para el RadioGroup y los RadioButtons
-            switch (optOperaciones.getCheckedRadioButtonId()) {
-                case R.id.optSuma:
-                    respuesta = num1 + num2;
-                    break;
-                case R.id.optResta:
-                    respuesta = num1 - num2;
-                    break;
-                case R.id.optMultiplicacion:
-                    respuesta = num1 * num2;
-                    break;
-                case R.id.optDivision:
-                    respuesta = num1 / num2;
-                    break;
-                case R.id.optPorcentaje:
-                    respuesta = (num1/num2)*num1;
-                    break;
-                case R.id.optExponenciacion:
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case R.id.optModulo:
-                    respuesta = num1%num2;
-                    break;
-                case R.id.optFactoreo:
-                    long factorial= 3;
-                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-factorial);
-                    break;
-
-        }
-            //Este es para el Spinner... -> ComboBox.
-            switch (cboOperaciones.getSelectedItemPosition()){
-                case 1: //suma
-                    respuesta = num1 + num2;
-                    break;
-                case 2: //resta
-                    respuesta = num1 - num2;
-                    break;
-                case 3: //multiplicacion
-                    respuesta = num1 * num2;
-                    break;
-                case 4: //division
-                    respuesta = num1 / num2;
-                    break;
-                case 5: //porcentaje
-                    respuesta = (num1/num2)*num1;
-                    break;
-                case 6: //exponenciacion
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case 7: //modulo
-                    respuesta = num1%num2;
-                    break;
-                case 8: //factoreo
-                    long factorial = 3;
-                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-factorial);
-                    break;
+        Spinner spnDepto = (Spinner)findViewById(R.id.spnDepto);
+        spnDepto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                spnMun.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,miDireccion.obtenerMunicipio(position) ));
+                Toast.makeText(getApplicationContext(), "Indice: "+ position, Toast.LENGTH_LONG).show();
             }
-            tempval = (TextView) findViewById(R.id.lblRespuesta);
-            tempval.setText("Respuesta: " + respuesta);
-        }catch (Exception err){
-            TextView temp = (TextView) findViewById(R.id.lblRespuesta);
-            temp.setText("POR FAVOR INGRESAR LOS NÚMEROS CORRESPONDIENTES.");
 
-           Toast.makeText(getApplicationContext(),"POR FAVOR INGRESAR LOS NÚMEROS.",Toast.LENGTH_LONG).show();
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Button btnObtenerPais = (Button)findViewById(R.id.btnObtenerPais);
+        btnObtenerPais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spinner spnPaises = (Spinner)findViewById(R.id.spnPaises);
+                spnPaises.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,miDireccion.obtenerPaises()));
+            }
+        });
+
 
     }
+
+
+
 }
