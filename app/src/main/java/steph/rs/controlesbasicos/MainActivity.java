e266ffb432dc928d32da8cbbc3c2f9e85e0e85ad
Hyung97 <tammyhyunglee@gmail.com>
2020-09-09 22:04:10 -0600

    Ya esta funcional

--- app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
+++ app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
@@ -1,23 +1,20 @@
 package steph.rs.controlesbasicos;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final EditText rNombre = (EditText)findViewById(R.id.etNombre);
         Button bEnviar = (Button)findViewById(R.id.btEnviar);
 
@@ -35,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
 
+    }
 }
