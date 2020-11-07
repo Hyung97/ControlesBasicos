4c2226debde160ee8ad5e42a55fbdcebe4b1fd40
Hyung97 <tammyhyunglee@gmail.com>
2020-11-06 19:55:41 -0600

    diseño

---
+++ app/src/main/java/steph/rs/controlesbasicos/DetailFood.java
@@ -0,0 +1,14 @@
+package steph.rs.controlesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_food);
+    }
+}
--- app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
+++ app/src/main/java/steph/rs/controlesbasicos/MainActivity.java
@@ -2,8 +2,11 @@ package steph.rs.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -11,89 +14,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button AppM = (Button) findViewById(R.id.Messaging_Apps);
+        AppM.setOnClickListener((View.OnClickListener) this);
+        Button PhoneM = (Button) findViewById(R.id.phoneM);
+        PhoneM.setOnClickListener((View.OnClickListener) this);
     }
 
-    public void calcular(View view) {
-        try {
-            RadioGroup optOperaciones = (RadioGroup) findViewById(R.id.optOperaciones);
-            Spinner cboOperaciones = (Spinner) findViewById(R.id.cboOperaciones);
-
-            TextView tempval = (TextView) findViewById(R.id.txtNum1);
-            double num1 = Double.parseDouble(tempval.getText().toString());
-
-            tempval = (TextView) findViewById(R.id.txtNum2);
-            double num2 = Double.parseDouble(tempval.getText().toString());
-
-            double respuesta = 0;
-            //Este es para el RadioGroup y los RadioButtons
-            switch (optOperaciones.getCheckedRadioButtonId()) {
-                case R.id.optSuma:
-                    respuesta = num1 + num2;
-                    break;
-                case R.id.optResta:
-                    respuesta = num1 - num2;
-                    break;
-                case R.id.optMultiplicacion:
-                    respuesta = num1 * num2;
-                    break;
-                case R.id.optDivision:
-                    respuesta = num1 / num2;
-                    break;
-                case R.id.optPorcentaje:
-                    respuesta = (num1/num2)*num1;
-                    break;
-                case R.id.optExponenciacion:
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case R.id.optModulo:
-                    respuesta = num1%num2;
-                    break;
-                case R.id.optFactoreo:
-                    long factorial= 3;
-                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-factorial);
-                    break;
-
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.Messaging_Apps :
+                Intent moveIntent = new Intent(MainActivity.this, Menu.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.phoneM:
+                String phoneNumber = "(+503)7849-3319";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
         }
-            //Este es para el Spinner... -> ComboBox.
-            switch (cboOperaciones.getSelectedItemPosition()){
-                case 1: //suma
-                    respuesta = num1 + num2;
-                    break;
-                case 2: //resta
-                    respuesta = num1 - num2;
-                    break;
-                case 3: //multiplicacion
-                    respuesta = num1 * num2;
-                    break;
-                case 4: //division
-                    respuesta = num1 / num2;
-                    break;
-                case 5: //porcentaje
-                    respuesta = (num1/num2)*num1;
-                    break;
-                case 6: //exponenciacion
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case 7: //modulo
-                    respuesta = num1%num2;
-                    break;
-                case 8: //factoreo
-                    long factorial = 3;
-                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-factorial);
-                    break;
-            }
-            tempval = (TextView) findViewById(R.id.lblRespuesta);
-            tempval.setText("Respuesta: " + respuesta);
-        }catch (Exception err){
-            TextView temp = (TextView) findViewById(R.id.lblRespuesta);
-            temp.setText("POR FAVOR INGRESAR LOS NÚMEROS CORRESPONDIENTES.");
-
-           Toast.makeText(getApplicationContext(),"POR FAVOR INGRESAR LOS NÚMEROS.",Toast.LENGTH_LONG).show();
-        }
-
     }
 }
